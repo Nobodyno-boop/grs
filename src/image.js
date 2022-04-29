fad68e1abd482a15260433e88ff5535a79cf0136
Nobody <17411508+Nobodyno-boop@users.noreply.github.com>
2022-04-29 21:40:51 +0200

    v1.0.1

--- src/image.js
+++ src/image.js
@@ -20,18 +20,17 @@ const addHexSymbol = (str) => {
  *
  * @param {number} w
  * @param {number} h
- * @param {string} c1
- * @param {string} c2
+ * @param {string[]} colors
  * @param {number?} angle
  * @returns
  */
-exports.createImage = (w, h, c1, c2, angle = 35) => {
+exports.createImage = (w, h, colors, angle = 45) => {
   let radians = angle * (Math.PI / 180);
   let sin = Math.sin(radians);
   let cos = Math.cos(radians);
   let lineLength = Math.abs(w * sin + Math.abs(w * cos));
   let center = [w / 2, h / 2];
-  // 0.78
+
   let coord = [
     center[0] - cos * 0.51 * lineLength,
     center[1] - sin * 0.51 * lineLength,
@@ -44,8 +43,12 @@ exports.createImage = (w, h, c1, c2, angle = 35) => {
 
   let gradient = ctx.createLinearGradient(...coord);
 
-  gradient.addColorStop(0, addHexSymbol(c1));
-  gradient.addColorStop(1, addHexSymbol(c2));
+  colors.forEach((color, index) => {
+    color = addHexSymbol(color);
+    // Thanks to Entarielle for this quick Math.
+    let average = index / (colors.length - 1);
+    gradient.addColorStop(average, color);
+  });
 
   ctx.fillStyle = gradient;
   ctx.fillRect(0, 0, w, h);
